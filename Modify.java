975aabea520b5d1c245655bb92e090c712be21ee
Sylvia778 <ruihua824@gmail.com>
2017-05-22 17:22:30 -0400

    7
    while(true)// dont change this line
    {
         System.out.print(i + " ");
    }

---
+++ Modify.java
@@ -0,0 +1,14 @@
+package assignement1;
+
+public class Modify {
+
+	public static void main(String[] args) {
+		//Modify the given code to get the output as 1  3  5 7 9
+		int i= 1;
+		while(i<10)// dont change this line
+		{
+		 System.out.print(i + " ");
+		 i+= 2;
+		} 
+		}
+	}
