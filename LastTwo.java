f10cdc2a054e2f906578bde192540f3de3a52757
Sylvia778 <ruihua824@gmail.com>
2017-05-21 23:13:33 -0400

    1
    write a code to print last 2 digits of a given number.

---
+++ LastTwo.java
@@ -0,0 +1,11 @@
+package assignement1;
+
+public class LastTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12345;
+		System.out.print(a%100);
+	}
+
+}
