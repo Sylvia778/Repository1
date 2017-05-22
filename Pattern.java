b51af0427cbc746468d207135aee2384e4533093
Sylvia778 <ruihua824@gmail.com>
2017-05-22 16:49:25 -0400

    6
    Write a code to print below pattern
    
    *
    * * *
    * * * * *
    * * * * * * *

---
+++ Pattern.java
@@ -0,0 +1,16 @@
+package assignement1;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+      for(int i=4; i>0;i--){
+    	  System.out.print("*");
+    	  for(int j=i; j<4; j++){
+    		  System.out.print("**");
+    	  }
+    	  System.out.println();
+      }
+	}
+}
